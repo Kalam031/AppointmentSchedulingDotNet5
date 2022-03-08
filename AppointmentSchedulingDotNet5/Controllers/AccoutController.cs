5489a685c26861b824b07d5599b815a2fdac8dee
Kalam <Kalam@DESKTOP-2URI2JC>
2022-03-08 22:25:12 +0600

    add migration to create identity table and create account controller

---
+++ AppointmentSchedulingDotNet5/Controllers/AccoutController.cs
@@ -0,0 +1,23 @@
+﻿using AppointmentSchedulingDotNet5.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentSchedulingDotNet5.Controllers
+{
+    public class AccoutController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccoutController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AppointmentSchedulingDotNet5/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppointmentSchedulingDotNet5.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Èmail { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name ="Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+}
